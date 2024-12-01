970deff58f634a622313fb9c7d04fb4837d20468
Maarten Engels <maartene@thedreamweb.eu>
2024-12-01 10:10:03 +0100

    Test: the total difference between on a one number list should be the difference between those two numbers

---
+++ Day01/NOTES.md
@@ -0,0 +1,14 @@
+# Pomodoro Technique - 📝 Notes from the journey 🍅 by 🍅
+
+## 🏷️ Labels
+
+- ✅ done
+- 🚧 WIP
+- ❌ ERROR
+- ⚠️ TODO
+
+## 🍅 Pomodoro 1: understand the problem at hand
+
+- ✅ Read description
+- ✅ Import input data
+- 🚧 Find the correct distance value for a list of one entry
--- Day01/Tests/Day01Tests/Day01Tests.swift
+++ Day01/Tests/Day01Tests/Day01Tests.swift
@@ -4,3 +4,15 @@ import Testing
 @Test func example() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
 }
+
+
+func totalDistanceBetweenLists(_ input: String) -> Int {
+    2
+}
+
+@Suite("To find the first star on Day01") struct Day01Tests {
+    @Test("the total difference between on a one number list should be the difference between those two numbers") func oneNumberList() {
+        let input = "1 3"
+        #expect(totalDistanceBetweenLists(input) == 2)
+    }
+}
