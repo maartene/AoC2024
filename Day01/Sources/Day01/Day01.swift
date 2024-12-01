8b8917869fe302ac68bfde40f048be3f8d0e1952
Maarten Engels <maartene@thedreamweb.eu>
2024-12-01 10:34:42 +0100

    Refactor: move  to correct location

--- Day01/Sources/Day01/Day01.swift
+++ Day01/Sources/Day01/Day01.swift
@@ -1,2 +1,20 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+func totalDistanceBetweenLists(_ input: String) -> Int {
+    var leftList = [Int]()
+    var rightList = [Int]()
+    
+    let lines = input.split(separator: "\n")
+    for line in lines {
+        let lists = line.split(separator: " ")
+            .compactMap { Int($0) }
+        
+        leftList.append(lists[0])
+        rightList.append(lists[1])
+    }
+    
+    leftList.sort()
+    rightList.sort()
+    
+    return zip(leftList, rightList).reduce(0) { partialResult, zipSequence in
+        partialResult + abs(zipSequence.0 - zipSequence.1)
+    }
+}
--- Day01/Tests/Day01Tests/Day01Tests.swift
+++ Day01/Tests/Day01Tests/Day01Tests.swift
@@ -1,27 +1,6 @@
 import Testing
 @testable import Day01
 
-func totalDistanceBetweenLists(_ input: String) -> Int {
-    var leftList = [Int]()
-    var rightList = [Int]()
-    
-    let lines = input.split(separator: "\n")
-    for line in lines {
-        let lists = line.split(separator: " ")
-            .compactMap { Int($0) }
-        
-        leftList.append(lists[0])
-        rightList.append(lists[1])
-    }
-    
-    leftList.sort()
-    rightList.sort()
-    
-    return zip(leftList, rightList).reduce(0) { partialResult, zipSequence in
-        partialResult + abs(zipSequence.0 - zipSequence.1)
-    }
-}
-
 @Suite("To find the first star on Day01") struct Day01Tests {
     @Test("the total difference between on a one line number list should be the difference between those two numbers", arguments: [
         ("1 3", 2),
