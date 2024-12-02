06aea4c22e013d5dcb3e3fd434eed804b2aba350
Maarten Engels <maartene@thedreamweb.eu>
2024-12-02 11:04:45 +0100

    R: move solution functions to correct file

--- Day02/Sources/Day02/Day02.swift
+++ Day02/Sources/Day02/Day02.swift
@@ -1,2 +1,35 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Shared
+
+func numberOfSafeReports(_ input: String) -> Int {
+    let reports = convertStringToIntMatrix(input)
+    
+    return
+        reports
+        .filter {
+            reportIsSafe($0)
+        }
+        .count
+}
+
+func reportIsSafe(_ report: [Int]) -> Bool {
+    // Are the differences in order?
+    for i in 0..<report.count - 1 {
+        let difference = abs(report[i] - report[i + 1])
+        if difference < 1 || difference > 3 {
+            return false
+        }
+    }
+
+    // Is the direction uniform?
+    let increasing = report[0] < report[1]
+    let decreasing = increasing == false
+    for i in 1..<report.count {
+        if increasing && report[i] < report[i - 1] {
+            return false
+        } else if decreasing && report[i] > report[i - 1] {
+            return false
+        }
+    }
+
+    return true
+}
--- Day02/Tests/Day02Tests/Day02Tests.swift
+++ Day02/Tests/Day02Tests/Day02Tests.swift
@@ -1,41 +1,6 @@
-import Shared
 import Testing
 
 @testable import Day02
-
-func numberOfSafeReports(_ input: String) -> Int {
-    let reports = convertStringToIntMatrix(input)
-    
-    return
-        reports
-        .filter {
-            reportIsSafe($0)
-        }
-        .count
-}
-
-func reportIsSafe(_ report: [Int]) -> Bool {
-    // Are the differences in order?
-    for i in 0..<report.count - 1 {
-        let difference = abs(report[i] - report[i + 1])
-        if difference < 1 || difference > 3 {
-            return false
-        }
-    }
-
-    // Is the direction uniform?
-    let increasing = report[0] < report[1]
-    let decreasing = increasing == false
-    for i in 1..<report.count {
-        if increasing && report[i] < report[i - 1] {
-            return false
-        } else if decreasing && report[i] > report[i - 1] {
-            return false
-        }
-    }
-
-    return true
-}
 
 @Suite("To find the first star on day 2") struct Day02Star1Tests {
     let exampleInput =
