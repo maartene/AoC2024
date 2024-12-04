5432f14c85425a467a9d21fc59bac9d4cf964e82
Maarten Engels <maartene@mac.com>
2024-12-04 06:20:47 +0100

    R: remove unneeded example test

--- Day04/Tests/Day04Tests/Day04Tests.swift
+++ Day04/Tests/Day04Tests/Day04Tests.swift
@@ -20,10 +20,6 @@ func countXMAS(in input: String) -> Int {
     return 0
 }
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-}
-
 @Suite("To find the first star on day 04") struct Day04StarOneTests {
     @Test("The word XMAS should appear 0 times in an empty input") func emptyArrayCase() {
         #expect(countXMAS(in: "") == 0)
