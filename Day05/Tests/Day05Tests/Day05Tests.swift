8ed66df512c9629a93dd8f5d6f5df8ad88c5ef12
Maarten Engels <maartene@thedreamweb.eu>
2024-12-05 08:32:51 +0100

    R: Introduce rules in function signature

--- Day05/Tests/Day05Tests/Day05Tests.swift
+++ Day05/Tests/Day05Tests/Day05Tests.swift
@@ -1,7 +1,7 @@
 import Testing
 @testable import Day05
 
-func isValidSequence(_ sequence: [Int]) -> Bool {
+func isValidSequence(_ sequence: [Int], rules: [Int: Int]) -> Bool {
     if sequence == [75,97,47,61,53] {
         return false
     }
@@ -10,16 +10,21 @@ func isValidSequence(_ sequence: [Int]) -> Bool {
 }
 
 @Suite("To get the first star on day 05") struct Day05StarOneTests {
+    let rules = [Int:Int]()
+    
     @Test("The following sequences should be considered valid", arguments: [
         [75,47,61,53,29],
         [97,61,53,29,13],
         [75,29,13]
     ]) func validSequences(sequence: [Int]) {
-        #expect(isValidSequence(sequence))
+        #expect(isValidSequence(sequence, rules: rules))
     }
     
-    @Test("The following sequence should not be considered valid") func invalidSequence() {
-        let sequence = [75,97,47,61,53]
-        #expect(isValidSequence(sequence) == false)
+    @Test("The following sequences should not be considered valid", arguments: [
+        [75,97,47,61,53],
+//        [61,13,29],
+//        [97,13,75,29,47]
+    ]) func invalidSequences(sequence: [Int]) {
+        #expect(isValidSequence(sequence, rules: rules) == false)
     }
 }
