b81b2cd5509f583d9f2680e730cdd4e0bca595f5
Maarten Engels <maartene@thedreamweb.eu>
2024-12-05 08:25:51 +0100

    T: The following sequence should be considered valid

--- Day05/Tests/Day05Tests/Day05Tests.swift
+++ Day05/Tests/Day05Tests/Day05Tests.swift
@@ -1,6 +1,17 @@
 import Testing
 @testable import Day05
 
+func isValidSequence(_ sequence: [Int]) -> Bool {
+    true
+}
+
 @Test func example() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+}
+
+@Suite("To get the first star on day 05") struct Day05StarOneTests {
+    @Test("The following sequence should be considered valid") func validSequences() {
+        let sequence = [75,47,61,53,29]
+        #expect(isValidSequence(sequence))
+    }
 }
