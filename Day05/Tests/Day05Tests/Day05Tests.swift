44c5e48c779d8ccd85a7800cf17a5d84aec222a2
Maarten Engels <maartene@thedreamweb.eu>
2024-12-05 08:39:47 +0100

    T: Convert rules input to into rules

--- Day05/Tests/Day05Tests/Day05Tests.swift
+++ Day05/Tests/Day05Tests/Day05Tests.swift
@@ -9,8 +9,22 @@ func isValidSequence(_ sequence: [Int], rules: [Int: Int]) -> Bool {
     return true
 }
 
+func convertInputToRulesAndSequences(_ input: String) -> (rules: [Int: Int], sequences: [Int]) {
+    let lines = input.split(separator: "\n").map(String.init)
+    
+    let rules: [Int: Int] = lines.reduce(into: [:]) { result, line in
+        let tokens = line.split(separator: "|")
+        let key = Int(tokens[0])!
+        let value = Int(tokens[1])!
+        
+        result[key] = value
+    }
+    
+    return (rules, [])
+}
+
 @Suite("To get the first star on day 05") struct Day05StarOneTests {
-    let rules = [Int:Int]()
+
     
     @Test("The following sequences should be considered valid", arguments: [
         [75,47,61,53,29],
@@ -27,4 +41,46 @@ func isValidSequence(_ sequence: [Int], rules: [Int: Int]) -> Bool {
     ]) func invalidSequences(sequence: [Int]) {
         #expect(isValidSequence(sequence, rules: rules) == false)
     }
+    
+    @Test("Convert rules input to into rules") func convertInputToRules() {
+        let input =
+        """
+        47|53
+        97|13
+        """
+        
+        let expectedRules = [
+            47: 53,
+            97: 13
+        ]
+        
+        #expect(convertInputToRulesAndSequences(input).rules == expectedRules)
+    }
+    
+    static let rulesInput =
+    """
+    47|53
+    97|13
+    97|61
+    97|47
+    75|29
+    61|13
+    75|53
+    29|13
+    97|29
+    53|29
+    61|53
+    97|53
+    61|29
+    47|13
+    75|47
+    97|75
+    47|61
+    75|61
+    47|29
+    75|13
+    53|13
+    """
+    
+    let rules = convertInputToRulesAndSequences(rulesInput).rules
 }
