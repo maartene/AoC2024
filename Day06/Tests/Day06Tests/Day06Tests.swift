ec24b1b2c2857e12d91fe3e6e194fec453ce4ac2
Maarten Engels <maartene@mac.com>
2024-12-06 06:48:52 +0100

    R: remove unneeded example test

--- Day06/Tests/Day06Tests/Day06Tests.swift
+++ Day06/Tests/Day06Tests/Day06Tests.swift
@@ -1,10 +1,6 @@
 import Testing
 @testable import Day06
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-}
-
 @Suite("To get the first star on day 06") struct Day06StarOneTests {
     @Test("On a map without obstacles, when the guard starts at the bottom, the number of visited distinct locations is the height of the map", arguments: [
         ("^", 1),
