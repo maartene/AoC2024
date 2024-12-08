f229f4013fddcb919e4808073040a5164b448875
Maarten Engels <maartene@thedreamweb.eu>
2024-12-08 13:19:17 +0100

    T: This map has two antinode positions

--- Day08/Sources/Day08/Day08.swift
+++ Day08/Sources/Day08/Day08.swift
@@ -1,3 +1,18 @@
+import Shared
+
 func calculateNumberOfAntinodePositions(in map: String) -> Int {
-    0
+    var nodes = [Vector: Character]()
+    let rows = map.split(separator: "\n").map(String.init)
+    
+    for y in 0 ..< rows.count {
+        let cols: [Character] = rows[y].map { $0 }
+        for x in 0 ..< cols.count {
+            let position = Vector(x: x, y: y)
+            if cols[x] != "." {
+                nodes[position] = cols[x]
+            }
+        }
+    }
+    
+    return nodes.count
 }
