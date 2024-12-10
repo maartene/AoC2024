56719f42b3d7e2ebdac7b27513a042ed41a0a69c
Maarten Engels <maartene@mac.com>
2024-12-10 09:21:32 +0100

    R: remove unneeded example test

--- Day10/Tests/Day10Tests/Day10Tests.swift
+++ Day10/Tests/Day10Tests/Day10Tests.swift
@@ -2,10 +2,6 @@ import Testing
 import Shared
 @testable import Day10
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-}
-
 @Suite("To get the first star on day 10") struct Day10StarOneTests {
     @Test("In first example given, when starting from the trailhead at the top left position, there should be only one trail available") func exampleOneStartingTopLeft() {
         let map = 
