4280e5527be64f621ac5fde68f87f94cb7a96093
Maarten Engels <maartene@thedreamweb.eu>
2024-12-14 10:08:45 +0100

    R: extract function to find robots in quadrant

--- Day14/Sources/Day14/Day14.swift
+++ Day14/Sources/Day14/Day14.swift
@@ -17,34 +17,33 @@ func safetyFactor(for input: String) -> Int {
             let row: [Character] = line.map { $0 }
             return row.map { Int(String($0)) }
         }
-
-    var robotsInQuadrant1 = 0
-    for row in 0 ..< robotState.count / 2 {
-        for column in 0 ..< robotState[row].count / 2 {
-            robotsInQuadrant1 += robotState[row][column] ?? 0
+    
+    func getRobotsInQuadrant(in robotState: [[Int?]], for quadrant: Int) -> Int {
+        let bounds: (rowStart: Int, rowEnd: Int, columnStart: Int, columnEnd: Int) = switch quadrant {
+        case 1: (rowStart: 0, rowEnd: robotState.count / 2, columnStart: 0, columnEnd: robotState[0].count / 2)
+        case 2: (rowStart: 0, rowEnd: robotState.count / 2, columnStart: 1 + robotState[0].count / 2, columnEnd: robotState[0].count)
+        case 3: (rowStart: 1 + robotState.count / 2, rowEnd: robotState.count, columnStart: 0, columnEnd: robotState[0].count / 2)
+        case 4: (rowStart: 1 + robotState.count / 2, rowEnd: robotState.count, columnStart: 1 + robotState[0].count / 2, columnEnd: robotState[0].count)
+        default:
+            fatalError("Quadrant should be 1...4 but was \(quadrant).")
         }
+        
+        var robotsInQuantrant = 0
+        
+        for row in bounds.rowStart ..< bounds.rowEnd {
+            for column in bounds.columnStart ..< bounds.columnEnd {
+                robotsInQuantrant += robotState[row][column] ?? 0
+            }
+        }
+        
+        return robotsInQuantrant
     }
     
-    var robotsInQuadrant2 = 0
-    for row in 0 ..< robotState.count / 2 {
-        for column in 1 + robotState[row].count / 2 ..< robotState[row].count {
-            robotsInQuadrant2 += robotState[row][column] ?? 0
-        }
-    }
+    let robotsInQuadrant1 = getRobotsInQuadrant(in: robotState, for: 1)
+    let robotsInQuadrant2 = getRobotsInQuadrant(in: robotState, for: 2)
+    let robotsInQuadrant3 = getRobotsInQuadrant(in: robotState, for: 3)
+    let robotsInQuadrant4 = getRobotsInQuadrant(in: robotState, for: 4)
     
-    var robotsInQuadrant3 = 0
-    for row in 1 + robotState.count / 2 ..< robotState.count {
-        for column in 0 ..< robotState[row].count / 2 {
-            robotsInQuadrant3 += robotState[row][column] ?? 0
-        }
-    }
-    
-    var robotsInQuadrant4 = 0
-    for row in 1 + robotState.count / 2 ..< robotState.count {
-        for column in 1 + robotState[row].count / 2 ..< robotState[row].count {
-            robotsInQuadrant4 += robotState[row][column] ?? 0
-        }
-    }
     
     return robotsInQuadrant1 * robotsInQuadrant2 * robotsInQuadrant3 * robotsInQuadrant4
 }
