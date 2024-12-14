ced1cc1cf73b4932d9499eed1eb50bac343327fe
Maarten Engels <maartene@thedreamweb.eu>
2024-12-14 09:49:39 +0100

    R: make calculation of safety factor explicit

--- Day14/Sources/Day14/Day14.swift
+++ Day14/Sources/Day14/Day14.swift
@@ -2,5 +2,10 @@
 // https://docs.swift.org/swift-book
 
 func safetyFactor(for input: String) -> Int {
-    12
+    let robotsInQuadrant1 = 1
+    let robotsInQuadrant2 = 3
+    let robotsInQuadrant3 = 4
+    let robotsInQuadrant4 = 1
+    
+    return robotsInQuadrant1 * robotsInQuadrant2 * robotsInQuadrant3 * robotsInQuadrant4
 }
