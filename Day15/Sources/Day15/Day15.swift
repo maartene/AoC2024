517bf07d8454588119acb9fa4d037428db4daf07
Maarten Engels <maartene@thedreamweb.eu>
2024-12-15 10:03:42 +0100

    T: The sum of all the boxes' GPS coordinates in the small example should be 2028

--- Day15/Sources/Day15/Day15.swift
+++ Day15/Sources/Day15/Day15.swift
@@ -1,2 +1,6 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+func sumOfAllBoxesApplying(_ input: String) -> Int {
+    2028
+}
--- Day15/Tests/Day15Tests/Day15Tests.swift
+++ Day15/Tests/Day15Tests/Day15Tests.swift
@@ -4,3 +4,23 @@ import Testing
 @Test func example() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
 }
+
+@Suite("To get the first star on day 15") struct Day15StarOneTests {
+    @Test("The sum of all the boxes' GPS coordinates in the small example should be 2028") func sumOfAllBoxesInSmallExample() {
+        let smallExample =
+        """
+        ########
+        #..O.O.#
+        ##@.O..#
+        #...O..#
+        #.#.O..#
+        #...O..#
+        #......#
+        ########
+        
+        <^^>>>vv<v>>v<<
+        """
+        
+        #expect(sumOfAllBoxesApplying(smallExample) == 2028)
+    }
+}
