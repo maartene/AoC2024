241996bfec4a09459867894c1b38a8d4a6392982
Maarten Engels <maartene@mac.com>
2024-12-16 07:11:03 +0100

    R: parameterize tests steps and turn tests for example maps

--- Day16/Tests/Day16Tests/Day16Tests.swift
+++ Day16/Tests/Day16Tests/Day16Tests.swift
@@ -49,8 +49,11 @@ let secondExampleMap =
         #expect(lowestPossibleScore(in: testcase.mapString) == testcase.expectedLowestScore) 
     }
 
-    @Test("The path with the lowest score in the first example maze has moving forward 36 times and 7 turns") func stepsAndTurnsForLowestScoringPathInFirstExample() {
-        #expect(stepsAndTurnsForLowestScore(in: firstExampleMap) == (36, 7))
+    @Test("The path with the lowest score in the example mazes should be as expected", arguments: [
+        (firstExampleMap, (36, 7)),
+        (secondExampleMap, (48, 11))
+    ]) func stepsAndTurnsForLowestScoringPathInFirstExample(testcase: (mapString: String, stepsAndTurns: (Int, Int))) {
+        #expect(stepsAndTurnsForLowestScore(in: testcase.mapString) == testcase.stepsAndTurns)
     }
 
     @Test("The path with the lowest score in the second example maze has moving forward 48 times and turning 11 times") func stepsAndTurnsForLowestScoringPathInSecondExample() {
