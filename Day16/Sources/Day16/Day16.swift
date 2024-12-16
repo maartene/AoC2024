0b19ea237cce6f7e3a3d1186516e4aa6c40e938b
Maarten Engels <maartene@mac.com>
2024-12-16 07:01:14 +0100

    R: use the stepsAndTurnsForLowestScore function to determine score

--- Day16/Sources/Day16/Day16.swift
+++ Day16/Sources/Day16/Day16.swift
@@ -1,5 +1,6 @@
 func lowestPossibleScore(in mapString: String) -> Int {
-    7036
+    let stepsAndTurns = stepsAndTurnsForLowestScore(in: mapString)
+    return stepsAndTurns.steps + stepsAndTurns.turns * 1000
 }
 
 func stepsAndTurnsForLowestScore(in mapString: String) -> (steps: Int, turns: Int) {
