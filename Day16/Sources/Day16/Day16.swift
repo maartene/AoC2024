59ccb4c33df71bbb6447362c819beee57b3a5248
Maarten Engels <maartene@mac.com>
2024-12-16 07:12:12 +0100

    R: introduce typealias StepsAndTurns for tuple

--- Day16/Sources/Day16/Day16.swift
+++ Day16/Sources/Day16/Day16.swift
@@ -1,9 +1,11 @@
+typealias StepsAndTurns = (steps: Int, turns: Int)
+
 func lowestPossibleScore(in mapString: String) -> Int {
     let stepsAndTurns = stepsAndTurnsForLowestScore(in: mapString)
     return stepsAndTurns.steps + stepsAndTurns.turns * 1000
 }
 
-func stepsAndTurnsForLowestScore(in mapString: String) -> (steps: Int, turns: Int) {
+func stepsAndTurnsForLowestScore(in mapString: String) -> StepsAndTurns {
     return if mapString.count >= 289 {
         (48, 11)
     } else {
--- Day16/Tests/Day16Tests/Day16Tests.swift
+++ Day16/Tests/Day16Tests/Day16Tests.swift
@@ -52,14 +52,7 @@ let secondExampleMap =
     @Test("The path with the lowest score in the example mazes should be as expected", arguments: [
         (firstExampleMap, (36, 7)),
         (secondExampleMap, (48, 11))
-    ]) func stepsAndTurnsForLowestScoringPathInFirstExample(testcase: (mapString: String, stepsAndTurns: (Int, Int))) {
+    ]) func stepsAndTurnsForLowestScoringPathInFirstExample(testcase: (mapString: String, stepsAndTurns: StepsAndTurns)) {
         #expect(stepsAndTurnsForLowestScore(in: testcase.mapString) == testcase.stepsAndTurns)
     }
-
-    @Test("The path with the lowest score in the second example maze has moving forward 48 times and turning 11 times") func stepsAndTurnsForLowestScoringPathInSecondExample() {
-        #expect(stepsAndTurnsForLowestScore(in: secondExampleMap) == (48, 11))
-    }
-
-
-
 }
