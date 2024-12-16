722265cd1b8025949b528b6a99f298fe8bfea7d7
Maarten Engels <maartene@mac.com>
2024-12-16 06:55:39 +0100

    T: The lowest score possible on the first map should be 7036

--- Day16/Tests/Day16Tests/Day16Tests.swift
+++ Day16/Tests/Day16Tests/Day16Tests.swift
@@ -1,6 +1,54 @@
 import Testing
 @testable import Day16
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+@Suite("To get the first star on day 16") struct Day16StarOneTests {
+    @Test("The lowest score possible on the first map should be 7036") func lowestScoreOnFirstMap() {
+        let firstExampleMap = 
+        """
+        ###############
+        #.......#....E#
+        #.#.###.#.###.#
+        #.....#.#...#.#
+        #.###.#####.#.#
+        #.#.#.......#.#
+        #.#.#####.###.#
+        #...........#.#
+        ###.#.#####.#.#
+        #...#.....#.#.#
+        #.#.#.###.#.#.#
+        #.....#...#.#.#
+        #.###.#.#.#.#.#
+        #S..#.....#...#
+        ###############
+        """
+
+        #expect(lowestPossibleScore(in: firstExampleMap) == 7036) 
+    }
+
+    // @Test("The lowest score possible on the second example map should be 11048") func lowestScoreOnSecondMap() {
+    //     let secondExampleMap = 
+    //     """
+    //     #################
+    //     #...#...#...#..E#
+    //     #.#.#.#.#.#.#.#.#
+    //     #.#.#.#...#...#.#
+    //     #.#.#.#.###.#.#.#
+    //     #...#.#.#.....#.#
+    //     #.#.#.#.#.#####.#
+    //     #.#...#.#.#.....#
+    //     #.#.#####.#.###.#
+    //     #.#.#.......#...#
+    //     #.#.###.#####.###
+    //     #.#.#...#.....#.#
+    //     #.#.#.#####.###.#
+    //     #.#.#.........#.#
+    //     #.#.#.#########.#
+    //     #S#.............#
+    //     #################
+    //     """
+
+    //     #expect(lowestPossibleScore(in: secondExampleMap) == 11048) 
+    // }
+
+
 }
