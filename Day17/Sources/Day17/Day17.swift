52660914839e8b85a14679ca57e29a9ff00c8e3d
Maarten Engels <maartene@mac.com>
2024-12-17 06:12:07 +0100

    R: collect output from indivual steps into output

--- Day17/Sources/Day17/Day17.swift
+++ Day17/Sources/Day17/Day17.swift
@@ -1,3 +1,5 @@
 func run(_ program: String) -> String {
-    "4,6,3,5,6,3,5,2,1,0"
+    let programOutput = [4,6,3,5,6,3,5,2,1,0].map(String.init)
+
+    return programOutput.joined(separator: ",")
 }
