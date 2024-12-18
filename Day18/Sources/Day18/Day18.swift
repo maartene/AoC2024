63139515ef2d2fd1ea965128a551a4bc49957d7f
Maarten Engels <maartene@mac.com>
2024-12-18 08:52:07 +0100

    R: simplify BFS function

--- Day18/Sources/Day18/Day18.swift
+++ Day18/Sources/Day18/Day18.swift
@@ -15,35 +15,25 @@ func shortestPath(through input: String, simulateUntilByte: Int = Int.max, mapSi
 }
 
 func BFS(start: Vector, destination: Vector, in memory: Set<Vector>, mapSize: Vector) -> Int? {
-    let rowCount = mapSize.y
-    let colCount = mapSize.x
-
-    // Richtingen om noord, zuid, oost, west te bewegen
-    let directions = [Vector(x: 0, y: 1), Vector(x: 1, y: 0), Vector(x: 0, y: -1), Vector(x: -1, y: 0)]
-
-    // Queue voor BFS en een set om bezochte punten bij te houden
-    var queue: [(Vector, Int)] = [(start, 0)]  // (huidige punt, afstand)
+    // Queue for BFS and a set to keep track of visited points
+    var queue: [(Vector, Int)] = [(start, 0)]  // (current point, distance)
     var visited: Set<Vector> = [start]
 
     while queue.isEmpty == false {
         let (current, distance) = queue.removeFirst()
 
-        // Check of het huidige punt de bestemming is
+        // Check if current point is the destination
         if current == destination {
             return distance
         }
 
-        // Probeer in alle richtingen te bewegen
-        for direction in directions {
-            let newX = current.x + direction.x
-            let newY = current.y + direction.y
-            let newPoint = Vector(x: newX, y: newY)
-
-            // Controleer binnen de grid grenzen en of het punt niet bezocht is
-            if newX >= 0, newY >= 0, newX < rowCount, newY < colCount,
-               memory.contains(Vector(x: newX, y: newY)) == false, visited.contains(newPoint) == false {
-                queue.append((newPoint, distance + 1))
-                visited.insert(newPoint)
+        // Try to move in all directions
+        for neighbour in current.neighbours {
+            // Check if its within grid and not an occupied location
+            if neighbour.x >= 0, neighbour.y >= 0, neighbour.x < mapSize.y, neighbour.y < mapSize.x,
+               memory.contains(neighbour) == false, visited.contains(neighbour) == false {
+                queue.append((neighbour, distance + 1))
+                visited.insert(neighbour)
             }
         }
     }
@@ -51,22 +41,7 @@ func BFS(start: Vector, destination: Vector, in memory: Set<Vector>, mapSize: Ve
     return nil
 }
 
-func printMap(memory: [Vector], mapSize: Vector) {
-    for y in 0 ..< mapSize.y {
-        var line = ""
-        for x in 0 ..< mapSize.x {
-            if memory.contains(Vector(x: x, y: y)) {
-                line += "#"
-            } else {
-                line += "."
-            }
-        }
-        print(line)
-    }
-}
-
 func findFirstBlockingByte(input: String, startingAt: Int, mapSize: Vector) -> String {
-    
     let lines = input.split(separator: "\n").map { String($0)}
     let entiryMemory = lines
         .map { Vector(from: $0) }
