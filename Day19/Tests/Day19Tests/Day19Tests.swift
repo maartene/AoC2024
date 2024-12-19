81541254196d1d0840109fe7bf2eacf6520cee19
Maarten Engels <maartene@thedreamweb.eu>
2024-12-19 10:18:12 +0100

    R: remove unneeded example test

--- Day19/Tests/Day19Tests/Day19Tests.swift
+++ Day19/Tests/Day19Tests/Day19Tests.swift
@@ -1,10 +1,6 @@
 import Testing
 @testable import Day19
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-}
-
 @Suite("To get the first star on day 19") struct Day19StarOneTests {
     @Test("Only 6 of 8 designs can be made using towels in the example input") func possibleDesignsInExampleInput() {
         let exampleInput =
