002bf9c9f372641f3b2c9efac26d65823962bdfe
Maarten Engels <maartene@thedreamweb.eu>
2024-12-19 08:51:59 +0100

    R: parse input and validate input (using stub) to get correct result

--- Day19/Sources/Day19/Day19.swift
+++ Day19/Sources/Day19/Day19.swift
@@ -1,3 +1,22 @@
+import Foundation
+
 func possibleDesigns(in input: String) -> Int {
-    6
+    var lines = input.split(separator: "\n")
+    let towelTypes = lines[0].split(separator: ",").map { String($0).trimmingCharacters(in: .whitespacesAndNewlines) }
+    lines.removeFirst()
+    
+    let designs = lines.map(String.init)
+    
+    return designs.filter {
+        isPossibleDesign(design: $0, towelTypes: towelTypes)
+    }.count
+}
+
+func isPossibleDesign(design: String, towelTypes: [String]) -> Bool {
+    switch design {
+    case "ubwu": false
+    case "bbrgwb": false
+    default:
+        true
+    }
 }
