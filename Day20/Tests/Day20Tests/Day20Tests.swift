357493f5db8169a915f5d7df71fe87a98b064a24
Maarten Engels <maartene@thedreamweb.eu>
2024-12-20 22:14:21 +0100

    T: There are a number of cheats in the example input that save exactly certain picoseconds

--- Day20/Tests/Day20Tests/Day20Tests.swift
+++ Day20/Tests/Day20Tests/Day20Tests.swift
@@ -44,7 +44,24 @@ let exampleInput =
 }
 
 @Suite("To get the second star on day 20") struct Day20StarTwoTests {
-    
+    @Test("There are a number of cheats in the example input that save exactly certain picoseconds", arguments: [
+          (76, 3),
+          (74, 4),
+          (72, 22),
+          (70, 12),
+          (68, 14),
+          (66, 12),
+          (64, 19),
+          (62, 20),
+          (60, 23),
+          (58, 25),
+          (56, 39),
+          (54, 29),
+          (52, 31),
+          (50, 32),
+      ]) func picoSecondsSavedInTheExampleInput(testCase: (numberOfSecondsSaved: Int, expectedNumberOfCheats: Int)) {
+          #expect(numberOfCheatsThatSaveSpecificNumberOfPicoSeconds(in: exampleInput, maxCheats: 20)[testCase.numberOfSecondsSaved] == testCase.expectedNumberOfCheats)
+      }
     
     @Test("There are 1011325 number of cheats in the actual input that saves at least 100 picoseconds") func picoSecondsSavedInActualInput() {
         #expect(numberOfCheatsThatSaveAtLeast(picoSeconds: 100, in: input, maxCheats: 20) == 1011325)
