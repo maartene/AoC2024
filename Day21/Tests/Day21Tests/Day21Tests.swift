8f145514a37d47b2e17f1f58886e5482b3345b05
Maarten Engels <maartene@thedreamweb.eu>
2024-12-21 10:17:56 +0100

    R: remove unneeded example test

--- Day21/Tests/Day21Tests/Day21Tests.swift
+++ Day21/Tests/Day21Tests/Day21Tests.swift
@@ -1,10 +1,6 @@
 import Testing
 @testable import Day21
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-}
-
 @Suite("To get the first star on day 21") struct Day21StarOneTests {
     @Test("The complexity factor of the example input is 126384") func complexityFactorOfExampleInput() {
         let exampleInput =
