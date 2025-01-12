4ed6050931cfa3551059b980f3e29d63bcacbf7f
Maarten Engels <maartene@thedreamweb.eu>
2025-01-12 09:49:09 +0100

    R: sets of three interconnected computers no longer hard coded

--- Day23/Sources/Day23/Day23.swift
+++ Day23/Sources/Day23/Day23.swift
@@ -4,37 +4,50 @@
 func numberOfSetsOfThreeInterconnectedComputersWithAT(_ input: String) -> Int {
     let connections = extractConnectionsFrom(input)
     
+    var setsOfThreeInterconnectedComputers = Set<Set<String>>()
     
+    let connectionsWithT = connections.filter { $0.key.contains("t") }
     
-    let setsOfThreeInterconnectedComputers: Set<Set<String>> = [
-        ["aq", "cg" ,"yn"],
-        ["aq", "vc" ,"wq"],
-        ["co", "de" ,"ka"],
-        ["co", "de" ,"ta"],
-        ["co", "ka" ,"ta"],
-        ["de", "ka" ,"ta"],
-        ["kh", "qp" ,"ub"],
-        ["qp", "td" ,"wh"],
-        ["tb", "vc" ,"wq"],
-        ["tc", "td" ,"wh"],
-        ["td", "wh" ,"yn"],
-        ["ub", "vc" ,"wq"],
-    ]
-    
+    for connectionWithT in connectionsWithT {
+        for connectedComputer1 in connectionWithT.value {
+            let connectedWithComputer1 = connections[connectedComputer1, default: []]
+            for connectedComputer2 in connectedWithComputer1 {
+                let connectedWithComputer2 = connections[connectedComputer2, default: []]
+                var interconnectedComputers = Set<String>()
+                interconnectedComputers.insert(connectionWithT.key)
+                interconnectedComputers.insert(connectedComputer1)
+                if connectedWithComputer2.contains(connectionWithT.key) {
+                    interconnectedComputers.insert(connectedComputer2)
+                }
+                if interconnectedComputers.count == 3 {
+                    setsOfThreeInterconnectedComputers.insert(interconnectedComputers)
+                }
+            }
+        }
+    }
+        
     return setsOfThreeInterconnectedComputers
         .filter { $0.contains(where: { $0.contains("t") }) }
         .count
 }
 
-private func extractConnectionsFrom(_ input: String) -> Set<Set<String>> {
+private func extractConnectionsFrom(_ input: String) -> [String: Set<String>] {
     let lines = input.split(separator: "\n")
     
-    var connections = Set<Set<String>>()
+    var connections = [String: Set<String>]()
     for line in lines {
         let connectionSplit = line.split(separator: "-").map { String($0) }
-        let connection = Set(connectionSplit)
-        connections.insert(connection)
+        
+        let from = connectionSplit[0]
+        let existingFrom = Set(connections[from] ?? [])
+        let to = connectionSplit[1]
+        connections[from] = existingFrom.union([to])
+        
+        let existingTo = connections[to] ?? []
+        connections[to] = existingTo.union([from])
     }
+    
+    print(connections.count)
     
     return connections
 }
