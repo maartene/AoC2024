9d5f8995be7b7efbaebbd529c138a852a0686c0c
Maarten Engels <maartene@thedreamweb.eu>
2025-01-12 09:30:26 +0100

    R: extract connections from input

--- Day23/Sources/Day23/Day23.swift
+++ Day23/Sources/Day23/Day23.swift
@@ -2,6 +2,10 @@
 // https://docs.swift.org/swift-book
 
 func numberOfSetsOfThreeInterconnectedComputersWithAT(_ input: String) -> Int {
+    let connections = extractConnectionsFrom(input)
+    
+    
+    
     let setsOfThreeInterconnectedComputers: Set<Set<String>> = [
         ["aq", "cg" ,"yn"],
         ["aq", "vc" ,"wq"],
@@ -20,4 +24,17 @@ func numberOfSetsOfThreeInterconnectedComputersWithAT(_ input: String) -> Int {
     return setsOfThreeInterconnectedComputers
         .filter { $0.contains(where: { $0.contains("t") }) }
         .count
+}
+
+private func extractConnectionsFrom(_ input: String) -> Set<Set<String>> {
+    let lines = input.split(separator: "\n")
+    
+    var connections = Set<Set<String>>()
+    for line in lines {
+        let connectionSplit = line.split(separator: "-").map { String($0) }
+        let connection = Set(connectionSplit)
+        connections.insert(connection)
+    }
+    
+    return connections
 }
