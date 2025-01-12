f804ae48a141afcecec2fb7032be06fc8a3a2e46
Maarten Engels <maartene@mac.com>
2025-01-12 20:49:40 +0100

    Day 24 scaffold

--- Day24/Tests/Day24Tests/Day24Tests.swift
+++ Day24/Tests/Day24Tests/Day24Tests.swift
@@ -4,3 +4,7 @@ import Testing
 @Test func example() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
 }
+
+@Suite("To get the first star on day 24") struct Day24StarOneTests {
+    
+}
