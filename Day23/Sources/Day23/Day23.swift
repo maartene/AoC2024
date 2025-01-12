7e471eb655cfa3a58fd27c01448f8c6ee60fcfd8
Maarten Engels <maartene@thedreamweb.eu>
2025-01-12 09:26:53 +0100

    R: filter sets of interconnected computers to the ones that have a 't' in them

--- Day23/Sources/Day23/Day23.swift
+++ Day23/Sources/Day23/Day23.swift
@@ -2,5 +2,22 @@
 // https://docs.swift.org/swift-book
 
 func numberOfSetsOfThreeInterconnectedComputersWithAT(_ input: String) -> Int {
-    7
+    let setsOfThreeInterconnectedComputers: Set<Set<String>> = [
+        ["aq", "cg" ,"yn"],
+        ["aq", "vc" ,"wq"],
+        ["co", "de" ,"ka"],
+        ["co", "de" ,"ta"],
+        ["co", "ka" ,"ta"],
+        ["de", "ka" ,"ta"],
+        ["kh", "qp" ,"ub"],
+        ["qp", "td" ,"wh"],
+        ["tb", "vc" ,"wq"],
+        ["tc", "td" ,"wh"],
+        ["td", "wh" ,"yn"],
+        ["ub", "vc" ,"wq"],
+    ]
+    
+    return setsOfThreeInterconnectedComputers
+        .filter { $0.contains(where: { $0.contains("t") }) }
+        .count
 }
