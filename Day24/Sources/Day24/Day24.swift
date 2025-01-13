24c1a3f4969427d1d14ab14ee8a37d9d6d899376
Maarten Engels <maartene@thedreamweb.eu>
2025-01-13 15:26:52 +0100

    R: extract relevant wires from the state

--- Day24/Sources/Day24/Day24.swift
+++ Day24/Sources/Day24/Day24.swift
@@ -1,18 +1,24 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 func numberValueResultingFromCircuit(_ input: String) -> Int {
-    let output = [
+    let state = [
+        "x00": 1,
+        "x01": 1,
+        "x02": 1,
+        "y00": 0,
+        "y01": 1,
+        "y02": 0,
         "z00": 0,
         "z01": 0,
         "z02": 1
     ]
     
-    
     // Construct the resulting number
+    let zKeys = state.keys.filter( { $0.hasPrefix("z") })
     var number = 0
-    for outputKey in output.keys.sorted(by: >) {
+    for stateKey in zKeys.sorted(by: >) {
         number = number << 1
-        number += output[outputKey, default: 0]
+        number += state[stateKey, default: 0]
     }
     
     return number
