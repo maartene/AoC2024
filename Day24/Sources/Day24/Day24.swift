7b53daef829ac2908367bc3a2350f425d7f3c4cb
Maarten Engels <maartene@thedreamweb.eu>
2025-01-13 15:29:23 +0100

    R: hard code first instruction

--- Day24/Sources/Day24/Day24.swift
+++ Day24/Sources/Day24/Day24.swift
@@ -1,17 +1,19 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 func numberValueResultingFromCircuit(_ input: String) -> Int {
-    let state = [
+    var state = [
         "x00": 1,
         "x01": 1,
         "x02": 1,
         "y00": 0,
         "y01": 1,
         "y02": 0,
-        "z00": 0,
         "z01": 0,
         "z02": 1
     ]
+    
+    // apply instructions to determine the end state
+    state["z00"] = state["x00"] == 1 && state["y00"] == 1 ? 1 : 0
     
     // Construct the resulting number
     let zKeys = state.keys.filter( { $0.hasPrefix("z") })
