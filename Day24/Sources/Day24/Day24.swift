840a45dbb7d7673069f2a6800754a16ebac8fdb8
Maarten Engels <maartene@thedreamweb.eu>
2025-01-13 15:24:22 +0100

    R: construct the resulting number from the output lines

--- Day24/Sources/Day24/Day24.swift
+++ Day24/Sources/Day24/Day24.swift
@@ -1,5 +1,19 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 func numberValueResultingFromCircuit(_ input: String) -> Int {
-    4
+    let output = [
+        "z00": 0,
+        "z01": 0,
+        "z02": 1
+    ]
+    
+    
+    // Construct the resulting number
+    var number = 0
+    for outputKey in output.keys.sorted(by: >) {
+        number = number << 1
+        number += output[outputKey, default: 0]
+    }
+    
+    return number
 }
