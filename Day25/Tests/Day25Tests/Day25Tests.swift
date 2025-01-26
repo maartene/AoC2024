fbe3c7b20ecfbc2de1f38fbb620c65bb4aac6981
Maarten Engels <maartene@thedreamweb.eu>
2025-01-26 16:37:42 +0100

    T: The number of unique lock/key pairs that fit in the example input should be 3

--- Day25/Tests/Day25Tests/Day25Tests.swift
+++ Day25/Tests/Day25Tests/Day25Tests.swift
@@ -4,3 +4,52 @@ import Testing
 @Test func example() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
 }
+
+@Suite("To get the first star on day 25") struct Day25StarOneTests {
+    @Test("The number of unique lock/key pairs that fit in the example input should be 3") func uniqueLockKeyPairs_inExampleInput() {
+        let exampleInput =
+        """
+        #####
+        .####
+        .####
+        .####
+        .#.#.
+        .#...
+        .....
+
+        #####
+        ##.##
+        .#.##
+        ...##
+        ...#.
+        ...#.
+        .....
+
+        .....
+        #....
+        #....
+        #...#
+        #.#.#
+        #.###
+        #####
+
+        .....
+        .....
+        #.#..
+        ###..
+        ###.#
+        ###.#
+        #####
+
+        .....
+        .....
+        .....
+        #....
+        #.#..
+        #.#.#
+        #####
+        """
+        
+        #expect(uniqueLockKeyPairsIn(exampleInput) == 3)
+    }
+}
