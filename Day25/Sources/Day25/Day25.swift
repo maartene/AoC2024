b659ae7773e6efb7628470934d697228aca4b4c4
Maarten Engels <maartene@thedreamweb.eu>
2025-01-26 18:49:28 +0100

    ⭐ First star of day 25

--- Day25/Sources/Day25/Day25.swift
+++ Day25/Sources/Day25/Day25.swift
@@ -5,110 +5,31 @@ import Algorithms
 import Shared
 
 func uniqueLockKeyPairsIn(_ input: String) -> Int {
-    let locks = getLocksFromInput(input)
-    print(locks)
+    let chunkStrings = input.split(separator: "\n\n").map { String($0) }
     
+    let chunks = chunkStrings.map { convertInputToMatrixOfCharacters($0) }
     
-    let keys = getKeysFromInput(input)
+    let combinations = chunks.combinations(ofCount: 2)
     
-    let combinations = product(locks, keys)
-    
-    var count = 0
-    for combination in combinations {
-        if combinationFits(combination.0, combination.1) {
-            count += 1
-        }
-    }
-    
-    
-    
-    return count
+    let fits = combinations.filter { combinationFits( $0[0], $0[1]) }
+    print(fits)
+    return fits.count
 }
 
-func combinationFits(_ lock: [Int], _ key: [Int]) -> Bool {
-    for element in zip(lock, key) {
-        if element.0 + element.1 > 5 {
-            return false
+func combinationFits(_ lock: [[Character]], _ key: [[Character]]) -> Bool {
+    for y in 0 ..< 7 {
+        for x in 0 ..< 5 {
+            if lock[y][x] == "#" && key[y][x] == "#" {
+                return false
+            }
         }
     }
     
     return true
 }
 
-func getKeysFromInput(_ input: String) -> [[Int]] {
-    let lines = input.split(separator: "\n").map { String($0) }
-
-    var keyStrings = [[String]]()
+func getChunks(_ input: String) -> [[[Character]]] {
+    let chunks = input.split(separator: "\n\n").map { String($0) }
     
-    var key = [String]()
-    for i in 0 ..< lines.count {
-        if lines[i] == "....." { // key starts
-            key = []
-        } else if lines[i] == "#####" { // key ends
-            keyStrings.append(key)
-        } else {
-            key.append(lines[i])
-        }
-    }
-    
-    keyStrings = keyStrings.filter { $0.isEmpty == false }
-    
-    let keyMatrices = keyStrings.map {
-        convertInputToMatrixOfCharacters($0.joined(separator: "\n"))
-    }
-        
-    var result = Set<[Int]>()
-    
-    for keyMatrix in keyMatrices {
-        var key = [0,0,0,0,0]
-        for x in 0 ..< keyMatrix[0].count {
-            for y in 0 ..< keyMatrix.count {
-                if keyMatrix[y][x] == "#" {
-                    key[x] += 1
-                }
-            }
-        }
-        result.insert(key)
-    }
-    
-    return Array(result)
-}
-
-func getLocksFromInput(_ input: String) -> [[Int]] {
-    let lines = input.split(separator: "\n").map { String($0) }
-
-    var lockStrings = [[String]]()
-    
-    var lock = [String]()
-    for i in 0 ..< lines.count {
-        if lines[i] == "#####" { // lock starts
-            lock = []
-        } else if lines[i] == "....." { // lock ends
-            lockStrings.append(lock)
-        } else {
-            lock.append(lines[i])
-        }
-    }
-    
-    lockStrings = lockStrings.filter { $0.isEmpty == false }
-    
-    let lockMatrices = lockStrings.map {
-        convertInputToMatrixOfCharacters($0.joined(separator: "\n"))
-    }
-        
-    var result = Set<[Int]>()
-    
-    for lockMatrix in lockMatrices {
-        var lock = [0,0,0,0,0]
-        for x in 0 ..< lockMatrix[0].count {
-            for y in 0 ..< lockMatrix.count {
-                if lockMatrix[y][x] == "#" {
-                    lock[x] += 1
-                }
-            }
-        }
-        result.insert(lock)
-    }
-    
-    return Array(result)
+    return chunks.map { convertInputToMatrixOfCharacters($0) }
 }
--- Day25/Tests/Day25Tests/Day25Tests.swift
+++ Day25/Tests/Day25Tests/Day25Tests.swift
@@ -48,4 +48,11 @@ import Testing
         
         #expect(uniqueLockKeyPairsIn(exampleInput) == 3)
     }
+    
+    @Test("The number of unique lock/key pairs that fit in the actual input should be 3320") func uniqueLockKeyPairs_inActualInput() {
+        #expect(uniqueLockKeyPairsIn(input) == 3320)
+    }
+    
+    // 9485 too high
+    // 5183 too high
 }
